ccb1d46177e09f77b85ad631d8abb0b0d14f0bc1
Noah <lauffern@gmail.com>
2022-04-06 11:51:25 +0000

    feat: admin dashboard renders product count as well as category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
 
   def show
+    @products = Product.all.count
+    @categories = Category.all.count
+
   end
 end
